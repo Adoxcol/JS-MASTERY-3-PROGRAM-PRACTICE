dac5f80fa84645716bc0d652a9cda6021ca7966a
Mahfuzur Rahman <99127099+Adoxcol@users.noreply.github.com>
2024-09-13 17:23:45 +0600

    git

--- y/app/(root)/page.tsx
+++ y/app/(root)/page.tsx
@@ -1,8 +1,30 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstname : 'Mahmood'};
   return (
-    <div>Home</div>
+    <section className ="home" >
+    <div className ='home-content'>
+      <header className="home-header">
+        <HeaderBox 
+        type ='greeting'
+        title ='Welcome'
+        user = {loggedIn?.firstname || 'Guest'}
+        subtext = 'Access and Manage your account and transactions'
+        />
+
+        <TotalBalanceBox
+        accounts = {[]}
+        totalBanks= {1}
+        totalCurrentBalance = {1250.35}
+        
+        />
+      </header>
+
+
+    </div>
+    </section>
   )
 }
 
