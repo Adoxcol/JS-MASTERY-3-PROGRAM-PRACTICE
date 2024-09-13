e6b0bfb8119e18c1f88469c41af3a00a1846642b
Mahfuzur Rahman <99127099+Adoxcol@users.noreply.github.com>
2024-09-13 15:29:30 +0600

    first commit

---
+++ y/app/(root)/layout.tsx
@@ -0,0 +1,21 @@
+
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  
+  return (
+   <main>
+    SIDEBAR 
+    {children}
+   </main>
+  );
+}
+
+
+  
+
