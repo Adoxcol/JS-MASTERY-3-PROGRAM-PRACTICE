b927df586ca1fe97aec1cd733d08f83a2189625d
Mahfuzur Rahman <99127099+Adoxcol@users.noreply.github.com>
2024-09-16 04:09:12 +0600

    implement full signup auth

--- y/lib/utils.ts
+++ y/lib/utils.ts
@@ -196,27 +196,16 @@ export const getTransactionStatus = (date: Date) => {
 };
 
 export const authFormSchema = (type: string) => z.object({
-  
-  
-  
-  //signup
-  username: type === 'sign-in' ? z.string().optional() : z.string().min(8),
+  // sign up
   firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
-  lastName: type === 'sign-in' ? z.string().optional() :  z.string().min(3),
+  lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
   address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
   city: type === 'sign-in' ? z.string().optional() : z.string().max(50),
   state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(2),
   postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
-  dob: type === 'sign-in' ? z.string().optional() : z.string().min(3),
-  ssn : type === 'sign-in' ? z.string().optional() : z.string().min(3),
-
-  //both
+ dob: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  // both
   email: z.string().email(),
   password: z.string().min(8),
-  
-
-
-
-
-
-});
+})
