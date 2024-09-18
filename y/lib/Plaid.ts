5d88ebfb5211aa280f945ab1dddafd721e0a190c
Mahfuzur Rahman <99127099+Adoxcol@users.noreply.github.com>
2024-09-18 20:56:10 +0600

    implement plaid link and dwolla

--- y/lib/Plaid.ts
+++ y/lib/Plaid.ts
@@ -1,10 +1,12 @@
-import { Configuration, PlaidApi,  PlaidEnvironments } from 'plaid';
+import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid';
 
-const configuration = new Configuration ({
+const configuration = new Configuration({
   basePath: PlaidEnvironments.sandbox,
   baseOptions: {
-    'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID, 
-    'PLAID-SECRET': process.env.PLAID_SECRET,
+    headers: {
+      'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
+      'PLAID-SECRET': process.env.PLAID_SECRET,
+    }
   }
 })
 
